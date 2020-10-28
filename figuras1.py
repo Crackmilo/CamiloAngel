56784484e80879269e6e8d26e257811a463482a1
Ricardo Miranda <Chevy06@users.noreply.github.com>
2020-10-27 18:12:36 -0700

    circulo

---
+++ figuras1.py
@@ -0,0 +1,36 @@
+import turtle
+from turtle import *
+from turtle import Screen
+
+def emptySquare():
+    pass  # TODO
+    
+def filledSquare():
+  pass  # TODO
+
+def emptyCircle():
+    t = turtle.Turtle()
+    t.circle(100)
+
+def filledCircle():
+    t = turtle.Turtle()
+    t.fillcolor('red')  # set the fillcolor
+    t.begin_fill()  # start the filling color 
+    t.circle(100)
+    t.end_fill()
+
+while True:
+    screen = Screen()
+    answer = screen.textinput("Next Game","1 - Square:")
+    if (answer is None):
+        break
+    elif (answer == '1'):
+        emptySquare()
+    elif (answer == '2'):
+        filledSquare()
+    elif (answer == '3'):
+        emptyCircle()
+    elif (answer == '4'):
+        filledCircle()
+  
+
